9990cfaf8b4bd1df21d3db594aeb21058d495dee
Farhan5402 <55521637+Farhan5402@users.noreply.github.com>
2021-04-11 21:25:10 +0400

    final

--- arrforapprovals.js
+++ arrforapprovals.js
@@ -9,19 +9,22 @@ function buildtable(data){
     var table = document.getElementById("myTable")
 
     for(var i = 0; i< data.length; i++){
-        var row =   `<tr>
+        if(data[i].status == "Pending"){
+            var row =   `<tr>
                         <td>${data[i].name}</td>
                         <td>${data[i].age}</td>
                         <td>${data[i].batch}</td>
                         <td>${data[i].status}</td>
                         <td>
-                            <div class="btn-group" role="group" aria-label="Basic example">
-                            <button type="button" class="btn btn-success">Approve</button>
-                            <button type="button" class="btn btn-danger">Deny</button>
+                            <div class="btn-group" role="group" aria-label="statuschanger">
+                            <button type="button" class="btn btn-success" id="approvebtn">Approve</button>
+                            <button type="button" class="btn btn-danger" id="denybtn">Deny</button>
                         </div>
                         </td>
                     </tr>`
-        table.innerHTML += row
+            table.innerHTML += row
+            
+        }
     }
 }
 
