a0c86d04535a55592e0e74ca0596ed6dd9fcda24
Farhan5402 <55521637+Farhan5402@users.noreply.github.com>
2021-04-11 18:14:15 +0400

    asd

---
+++ arrforapprovals.js
@@ -0,0 +1,23 @@
+var initlist = [
+    {name: "Farhan Sadiq", age:19, batch:2023, status:"Pending"},
+    {name: "Anomaly", age:27, batch:2012, status:"Pending"},
+    {name: "Get Right", age:32, batch:2002, status:"Pending"},
+    {name: "Natus Vincere", age:50, batch:2000, status:"Pending"}
+]
+
+function buildtable(data){
+    var table = document.getElementById("myTable")
+
+    for(var i = 0; i< data.length; i++){
+        var row =   `<tr>
+                        <td>${data[i].name}</td>
+                        <td>${data[i].age}</td>
+                        <td>${data[i].batch}</td>
+                        <td>${data[i].status}</td>
+
+                    </tr>`
+        table.innerHTML += row
+    }
+}
+
+buildtable(initlist)
