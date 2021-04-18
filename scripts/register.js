fe8b3743a03613af0fc0ce061757b68de0ebf88f
amarjamal <amarabjamal@gmail.com>
2021-04-18 22:26:06 +0800

    add message on wrong passwords input

--- scripts/register.js
+++ scripts/register.js
@@ -40,3 +40,24 @@ function loadFacultySelector()
         document.getElementById("faculty").add(option);
     })
 }
+
+function verifyPassword() {  
+    var pw = document.getElementById('password').value; 
+    var check = document.getElementById('cpassword').value; 
+
+    if(pw == "") {  
+        document.getElementById("passwordHelpBlock").innerHTML = "**Fill the password please!";  
+        return false;  
+    }  
+
+   //minimum password length validation  
+    if(pw.length < 8) {  
+        document.getElementById("passwordHelpBlock").innerHTML = "Must be atleast 8 characters";  
+        return false;  
+    }  
+    
+  //maximum length of password validation  
+    if(pw.length > 20) {  
+        document.getElementById("passwordHelpBlock").innerHTML = "Must not exceed 20 characters";  
+        return false;  
+}  
