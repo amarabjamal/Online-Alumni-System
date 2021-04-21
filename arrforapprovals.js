fd7b80344883d802bd3b44da965810b6129737e1
Farhan5402 <55521637+Farhan5402@users.noreply.github.com>
2021-04-21 23:28:14 +0400

    login button and other changes

--- arrforapprovals.js
+++ arrforapprovals.js
@@ -1,12 +1,12 @@
 var initlist = [
-    {name: "Farhan Sadiq", age:19, batch:2023, status:"Pending"},
-    {name: "Anomaly", age:27, batch:2012, status:"Pending"},
-    {name: "Get Right", age:32, batch:2002, status:"Pending"},
-    {name: "Natus Vincere", age:50, batch:2000, status:"Pending"},
-    {name: "Sayed", age:19, batch:2024, status:"Approved"},
-    {name: "Boi", age:19, batch:2025, status:"Approved"},
-    {name: "Simple", age:24, batch:2015, status:"Denied"},
-    {name: "Hobbit", age:20, batch:2019, status:"Denied"},
+    {name: "Farhan Sadiq", email:"thisisanemail@gmail.com", age:19, batch:2023, status:"Pending"},
+    {name: "Anomaly", email:"thisisanemail@yahoo.com", age:27, batch:2012, status:"Pending"},
+    {name: "Get Right", email:"thisisanemail@hotmail.com", age:32, batch:2002, status:"Pending"},
+    {name: "Natus Vincere", email:"thisisanemail@outlook.com", age:50, batch:2000, status:"Pending"},
+    {name: "Sayed", email:"thisisanemail@live.com", age:19, batch:2024, status:"Approved"},
+    {name: "Boi", email:"thisisanemail@yahoo.com", age:19, batch:2025, status:"Approved"},
+    {name: "Simple", email:"thisisanemail@gmail.com", age:24, batch:2015, status:"Denied"},
+    {name: "Hobbit", email:"thisisanemail@hotmail.com", age:20, batch:2019, status:"Denied"},
 
 ]
 
@@ -17,6 +17,7 @@ function buildtable(data){
         if(data[i].status == "Pending"){
             var row =   `<tr>
                         <td>${data[i].name}</td>
+                        <td>${data[i].email}</td>
                         <td class="text-center">${data[i].age}</td>
                         <td class="text-center">${data[i].batch}</td>
                         <td class="text-center">${data[i].status}</td>
