572fdf3552b74a514280cb9a935e9142443a48fb
aisyahatirah <fancyonce21@gmail.com>
2021-04-22 23:46:27 +0800

    modify alumni page, add javascript files

---
+++ suggestion.js
@@ -0,0 +1,23 @@
+
+let suggestion = [
+    "Siti Aisyah Atirah",
+    "Amirah",
+    "Amar",
+    "Imtihan",
+    "Farhan",
+    "Zeyad",
+    "Aisyah",
+    "Atirah",
+    "Amin",
+    "Amir",
+    "Anisah"
+];
+
+let yearSuggestion = [
+    "1990",
+    "1991",
+    "1992",
+    "1993",
+
+    "2001"
+];
