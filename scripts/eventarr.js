700da931b891ea06cecd633533d273145508cbbf
Farhan5402 <55521637+Farhan5402@users.noreply.github.com>
2021-04-27 21:07:39 +0400

    refined faculty choice

--- scripts/eventarr.js
+++ scripts/eventarr.js
@@ -1,6 +1,6 @@
 var initlist = [
     {ename: "Alumi Meet up 2021", venue: "Microsoft Teams", brief:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultricies sem quis nisl auctor ornare. Morbi et porttitor nulla, lacinia bibendum felis. Aenean quam velit, ultrices nec placerat nec, fringilla ac leo. Praesent euismod non turpis eget posuere. Fusce elementum.", date:"12-02-2021"},
-    {ename: "Networking event 2021", venue: "FSKTM", brief:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultricies sem quis nisl auctor ornare. Morbi et porttitor nulla, lacinia bibendum felis. Aenean quam velit, ultrices nec placerat nec, fringilla ac leo. Praesent euismod non turpis eget posuere. Fusce elementum.", date:"30-03-2021"}
+    {ename: "Networking event 2021", venue: "Faculty of Computer Science and Information Technology", brief:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultricies sem quis nisl auctor ornare. Morbi et porttitor nulla, lacinia bibendum felis. Aenean quam velit, ultrices nec placerat nec, fringilla ac leo. Praesent euismod non turpis eget posuere. Fusce elementum.", date:"30-03-2021"}
     
 ]
 
