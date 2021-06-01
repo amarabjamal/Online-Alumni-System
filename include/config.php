d4f11e8a00171f17002f064ad9f329b99e094e56
Farhan5402 <55521637+Farhan5402@users.noreply.github.com>
2021-06-01 18:51:36 +0400

    first

---
+++ include/config.php
@@ -0,0 +1,22 @@
+<?php
+//Step 1: Connecting to a Database using PDO API
+// modify these variables for your installation
+try {	
+	$connectionString = "mysql:host=localhost;dbname=wptest1";
+	$databaseUsername = 'user1';
+	$databasePassword = 'user1abc';
+
+	$pdo = new PDO($connectionString, $databaseUsername, $databasePassword);
+	// set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+    echo "Database connected successfully <br>"; 
+	
+    }
+catch(PDOException $e)
+    {
+    echo "Database connection failed: " . $e->getMessage();
+}
+
+
+?>
