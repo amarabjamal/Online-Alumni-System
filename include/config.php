34aa6bc6e00e225ead0e08ed45044d3722bc86bc
amarjamal <amarabjamal@gmail.com>
2021-06-09 15:59:19 +0800

    add config file

---
+++ include/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$db_name = "alumni_system";
+$username = "alumni_access";
+$password = "yThZyu3a9lFqBfDz";
+
+try{
+    $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+}catch(PDOException $exception){
+    echo "Connection error: " . $exception->getMessage();
+    die();
+}
+
+?>
