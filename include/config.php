4b38b37d835fac5bcb788d382e1a41f4da8ad06e
amarjamal <amarabjamal@gmail.com>
2021-06-09 21:17:37 +0800

    set error attribute

--- include/config.php
+++ include/config.php
@@ -7,6 +7,7 @@ $password = "yThZyu3a9lFqBfDz";
 
 try{
     $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $exception){
     echo "Connection error: " . $exception->getMessage();
     die();
