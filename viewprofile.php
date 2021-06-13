9aeb337dc280a84a686e6459506f62bddd4a1b75
aisyahatirah <fancyonce21@gmail.com>
2021-06-13 15:20:30 +0800

    remove phone number

--- viewprofile.php
+++ viewprofile.php
@@ -92,7 +92,6 @@ $conn = null;
                     <p class="about-me">About Me</p>
                     <p><?php echo htmlspecialchars($f['faculty']); ?></p>
                     <p><?php echo htmlspecialchars($row['grad_year']); ?></p>
-                    <p>Phone Number</p>
                 </div>
             </div>
         </div>
@@ -210,48 +209,8 @@ $conn = null;
 
     <!-- ===================================== Start Footer Area ===================================== -->
 
-    <footer id="footer" class="mt-auto">
-        <div class="footer-link-section">
-            <div class="container">
-                <div class="row justify-content-left">
-                    <div class="col-sm-6 col-md-4 item">
-                        <h3>Contact</h3>
-                        <ul>
-                            <li><a href="#">Find Us</a></li>
-                            <li><a href="#">FAQ</a></li>
-                            <li><a href="#">Help</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-6 col-md-4 item">
-                        <h3>Links</h3>
-                        <ul>
-                            <li><a href="#">About UM</a></li>
-                            <li><a href="#">Study @ UM</a></li>
-                            <li><a href="#">General Enquiry</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-6 col-md-4 item">
-                        <h3>Follow Us</h3>
-                        <ul>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#">Instagram</a></li>
-                            <li><a href="#">Twitter</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Copyrights -->
-        <div class="copyright py-4">
-            <div class="container text-center">
-                <p class="mb-0 py-2">&copy;
-                    <script>document.write(new Date().getFullYear())</script> UM Alumni All rights reserved.
-                </p>
-            </div>
-        </div>
-    </footer>
-
+    <?php include_once("footer.php"); ?>
+    
     <!-- ===================================== End Footer Area ===================================== -->
 
 </body>
