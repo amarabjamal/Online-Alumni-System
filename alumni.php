8955bf6f2dcab65e7c1ebffc80ba0e2e5545d502
aisyahatirah <fancyonce21@gmail.com>
2021-06-15 22:21:09 +0800

    change the landing header

--- alumni.php
+++ alumni.php
@@ -130,34 +130,29 @@ $conn = null;
         <!-- ROW FOR HEADER -->
         <div class="row graduate-text">
             <!-- header -->
-            <div class="col-8 alumni-text">
+            <div class="col  alumni-text">
                 <h1>Alumni</h1>
                 <p>(n.) former student or pupil of a school, college, or university. </p>
-                <form action="viewprofile.php" method="GET">
-                    <div class="search-box">
-                        <input type="text" name="search" placeholder="Search for alumni..">
+                <div class="search-box">
+                    <input type="text" placeholder="Search for alumni..">
 
-                        <div class="autocom-box"></div>
+                    <div class="autocom-box"></div>
 
-                        <div class="search-icon" type="submit" name="submit"  onclick="displayPage()">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-search bg-transparent text-white" viewBox="0 0 16 16">
-                                <path
-                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                            </svg>
-                        </div>
+                    <div class="search-icon" onclick="displayPage()">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-search bg-transparent text-white" viewBox="0 0 16 16">
+                            <path
+                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        </svg>
                     </div>
-                </form>
                 </div>
             </div>
             <!-- graduate image -->
-            <div class="col-4 d-none d-lg-block graduate">
+            <div class="col d-none d-lg-block graduate">
                 <img src="images/graduate.png" alt="graduate">
             </div>
         </div>
-
-
-
+        
         <!-- ROW FOR INDICATOR -->
         <div class="row indi-box">
             <!-- column untuk indicator -->
