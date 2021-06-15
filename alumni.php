caa6fba21f14887b934a5325b00965d9658089ed
aisyahatirah <fancyonce21@gmail.com>
2021-06-15 21:45:01 +0800

    adjust footer

--- alumni.php
+++ alumni.php
@@ -85,7 +85,7 @@ $conn = null;
     <link href="./node_modules/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
-<body>
+<body class="d-flex flex-column h-100">
     <!-- Bootstrap -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
