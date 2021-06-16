2e0a1c819722afe147055d1f58d04d0320e4b74f
aisyahatirah <fancyonce21@gmail.com>
2021-06-16 09:15:56 +0800

    adding autocomplete feature part 1

--- alumni.php
+++ alumni.php
@@ -124,8 +124,8 @@ $conn = null;
                     <div class="search-box">
                         <input type="text" name="full_name" placeholder="Search for alumni..">
 
-                        <div class="autocom-box"></div>
-
+                        <!-- <div class="autocom-box"></div> -->
+                        
                         <button type="submit" name="submit" class="search-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-search bg-transparent text-white" viewBox="0 0 16 16">
@@ -133,9 +133,9 @@ $conn = null;
                                     d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                             </svg>
                         </button>
-                        
                     </div>
                 </form>
+                <div></div>
                 
             </div>
             <!-- graduate image -->
